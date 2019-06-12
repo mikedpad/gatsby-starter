152ccf3861b17fe49a7622a93eb01ca777ee680d
Michael Daniel Padilla <michaeldpadilla@gmail.com>
2019-06-12 13:58:00 -0700

    Blog page changes using newer react / gatsby hooks

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,33 +1,32 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/Layout';
 import PostPreview from '../components/Blog/PostPreview';
 
-const Blog = ({
-  data: {
+const Blog = () => {
+  const {
     allMarkdownRemark: { edges },
-  },
-}) => (
-  <Layout>
-    {edges.map(({ node }) => (
-      <PostPreview key={node.id} {...node} />
-    ))}
-  </Layout>
-);
-
-export default Blog;
-
-export const blogQuery = graphql`
-  query BlogQuery {
-    allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "^//src/markdown/blog/" } }
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
-      edges {
-        node {
-          ...PostPreviewFragment
+  } = useStaticQuery(graphql`
+    query BlogQuery {
+      allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "^//src/markdown/blog/" } }
+        sort: { fields: [frontmatter___date], order: DESC }
+      ) {
+        edges {
+          node {
+            ...PostPreviewFragment
+          }
         }
       }
     }
-  }
-`;
+  `);
+  return (
+    <Layout>
+      {edges.map(({ node }) => (
+        <PostPreview key={node.id} {...node} />
+      ))}
+    </Layout>
+  );
+};
+
+export default Blog;
